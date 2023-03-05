a2b49320d1ef28e2040e2d5d2beb04f1f57201cc
evan-lloyd-git <evanlloyd@cmail.carleton.ca>
2023-03-05 06:03:01 -0500

    Work in progress user service, still receiving some errors.

--- src/main/java/main/service/UserService.java
+++ src/main/java/main/service/UserService.java
@@ -1,4 +1,26 @@
 package main.service;
 
-public class UserService {
+import main.model.OrderModel;
+import main.model.ShoppingCartModel;
+
+import java.util.List;
+
+public interface UserService {
+
+    void saveOrder(String username, OrderModel orderModel);
+    void removeOrder(String username, Integer id);
+    List<OrderModel> getOrderList(String username);
+
+
+    void setCart(String username, ShoppingCartModel shoppingCartModel);
+    void removeCart(String username);
+    ShoppingCartModel getShoppingCart(String username);
+
+
+    void updateShippingAddress(String username, String shippingAddress);
+    String getShippingAddress(String username);
+    void updateBilling(String username, String billingAddress);
+    String getBillingAddress(String username);
+    void updateEmail(String username, String email);
+    String getEmail(String username);
 }
